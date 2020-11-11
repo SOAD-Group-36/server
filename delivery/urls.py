770929b94461cdd56ec567c1afe293a747f00408
Tanishq <tanishqvilas.k18@iiits.in>
2020-11-11 21:08:44 +0530

    Logistic services api views added

--- delivery/urls.py
+++ delivery/urls.py
@@ -1,1 +1,10 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework import routers
+from delivery import views
+
+router = routers.DefaultRouter()
+router.register(r'Logisticservices', views.LogisticViewset)
+
+urlpatterns = [
+    path("logisticservices/", include(router.urls)),
+]
