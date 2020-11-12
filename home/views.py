7e27248993e1f45c1514084558a7d2d130cb01cc
Ashutosh Chauhan <masterashu@live.in>
2020-11-12 15:09:07 +0530

    Adding Product Search Feature and UI Changes

--- home/views.py
+++ home/views.py
@@ -1,10 +1,17 @@
 from django.views import View
 from django.shortcuts import render
+from django.db.models import Q
 
 from products.models import Product
 
 
 class HomePage(View):
     def get(self, request):
-        context = {'products': Product.objects.all()}
+        products = Product.objects.all()
+        if request.GET.get('query', None):
+            query = request.GET['query']
+            products = products.filter(
+                Q(name__contains=query) | Q(description__contains=query) & Q(stock__gt=0)
+            )
+        context = {'products': products, 'exists': products.count() > 0}
         return render(request, 'home/index.html', context=context)
