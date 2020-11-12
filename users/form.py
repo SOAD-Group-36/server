b97590bffd6cc42acfe850c94d2e40e79859aec2
Ashutosh Chauhan <masterashu@live.in>
2020-11-12 18:36:15 +0530

    Update form.py

--- users/form.py
+++ users/form.py
@@ -5,4 +5,4 @@ from users.models import User
 class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email', 'mobile')
